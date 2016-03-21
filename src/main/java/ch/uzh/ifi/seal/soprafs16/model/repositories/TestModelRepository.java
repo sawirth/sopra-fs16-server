92374940a69d9e4185f90ed55e6b2161808b5a47
Sandro Wirth <s.wirth303@gmail.com>
2016-03-21 23:22:32 +0100

    just testing the framework

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/model/repositories/TestModelRepository.java
@@ -0,0 +1,8 @@
+package ch.uzh.ifi.seal.soprafs16.model.repositories;
+
+import ch.uzh.ifi.seal.soprafs16.model.TestModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TestModelRepository extends CrudRepository<TestModel, Long>{
+
+}
