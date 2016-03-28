6a3b549c0f62b55355d52ee8656cadfb95391a76
Ada Langenfeld <ada.langenfeld@gmail.com>
2016-03-28 18:46:55 +0200

    Create BoardServiceController and initial board classes.

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/controller/BoardServiceController.java
@@ -0,0 +1,7 @@
+package ch.uzh.ifi.seal.soprafs16.controller;
+
+/**
+ * Created by Ada on 28.03.2016.
+ */
+public class BoardServiceController {
+}
---
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/model/Board.java
@@ -0,0 +1,7 @@
+package ch.uzh.ifi.seal.soprafs16.model;
+
+/**
+ * Created by Ada on 28.03.2016.
+ */
+public class Board {
+}
