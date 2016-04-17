403ef18ba739aa14009bf7f0e0c5fccd2c6557b6
David Lay <david@lay-world.de>
2016-04-17 11:55:07 +0200

    add class roundFinisher with TakeAll event and tests #6

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/model/roundFinisher/RoundFinisherTakeAll.java
@@ -0,0 +1,24 @@
+package ch.uzh.ifi.seal.soprafs16.model.roundFinisher;
+
+import ch.uzh.ifi.seal.soprafs16.constant.TreasureType;
+import ch.uzh.ifi.seal.soprafs16.model.Game;
+import ch.uzh.ifi.seal.soprafs16.model.RoundFinisher;
+import ch.uzh.ifi.seal.soprafs16.model.Treasure;
+import ch.uzh.ifi.seal.soprafs16.model.Wagon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by David on 16.04.2016.
+ */
+public class RoundFinisherTakeAll extends RoundFinisher {
+    @Override
+    public void finishRound(Game game) {
+        int i=0;
+        while(game.getTrain().get(i).hasMarshal()==false){
+            i++;
+        }
+        game.getTrain().get(i).getLowerLevel().getTreasures().add(new Treasure(1000, TreasureType.CASHBOX));
+    }
+}
--- src/test/java/ch/uzh/ifi/seal/soprafs16/roundFinisher/RoundFinisherTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs16/roundFinisher/RoundFinisherTest.java
@@ -3,6 +3,7 @@ package ch.uzh.ifi.seal.soprafs16.roundFinisher;
 import ch.uzh.ifi.seal.soprafs16.constant.TreasureType;
 import ch.uzh.ifi.seal.soprafs16.model.*;
 import ch.uzh.ifi.seal.soprafs16.model.roundFinisher.RoundFinisherCrane;
+import ch.uzh.ifi.seal.soprafs16.model.roundFinisher.RoundFinisherTakeAll;
 import org.junit.Test;
 import org.junit.Assert;
 
@@ -18,6 +19,7 @@ import static org.hamcrest.Matchers.not;
 public class RoundFinisherTest {
 
     private RoundFinisherCrane roundFinisherCrane = new RoundFinisherCrane();
+    private RoundFinisherTakeAll roundFinisherTakeAll = new RoundFinisherTakeAll();
 
     @Test
     public void testRoundFinisherCrane(){
@@ -37,34 +39,48 @@ public class RoundFinisherTest {
         Assert.assertThat(train.get(1).getLowerLevel().getUsers().size(), is(1));
     }
 
+    @Test
+    public void testRoundFinisherTakeAll(){
+        Game game = createGame();
+        roundFinisherTakeAll.finishRound(game);
+
+        //Marshal is on first wagon, treasuresList should be of size 4
+        Assert.assertThat(game.getTrain().get(0).getLowerLevel().getTreasures().size(), is(4));
+        Assert.assertThat(game.getTrain().get(0).getLowerLevel().getTreasures().get(3).getType(), is(TreasureType.CASHBOX));
+    }
+
     private Game createGame(){
         Game game = new Game();
         List<Wagon> train = new ArrayList<>();
-        List<Treasure> treasures = new ArrayList<>();
-        treasures.add(new Treasure(250, TreasureType.MONEYBAG));
-        treasures.add(new Treasure(1000, TreasureType.CASHBOX));
-        treasures.add(new Treasure(500, TreasureType.DIAMOND));
 
         List<User> users1 = new ArrayList<>();
         users1.add(new User());
         users1.add(new User());
-        Wagon wagon1 = new Wagon(treasures,false);
+        Wagon wagon1 = new Wagon(createTreasures(),true);
         wagon1.getUpperLevel().setUsers(users1);
         train.add(wagon1);
 
         List<User> users2 = new ArrayList<>();
         users2.add(new User());
-        Wagon wagon2 = new Wagon(treasures,false);
+        Wagon wagon2 = new Wagon(createTreasures(),false);
         wagon2.getLowerLevel().setUsers(users2);
         train.add(wagon2);
 
         List<User> users3 = new ArrayList<>();
         users3.add(new User());
-        Wagon wagon3 = new Wagon(treasures,false);
+        Wagon wagon3 = new Wagon(createTreasures(),false);
         wagon3.getUpperLevel().setUsers(users3);
         train.add(wagon3);
 
         game.setTrain(train);
         return game;
+    }
+
+    private List<Treasure> createTreasures(){
+        List<Treasure> treasures = new ArrayList<>();
+        treasures.add(new Treasure(250, TreasureType.MONEYBAG));
+        treasures.add(new Treasure(1000, TreasureType.CASHBOX));
+        treasures.add(new Treasure(500, TreasureType.DIAMOND));
+        return treasures;
     }
 }
