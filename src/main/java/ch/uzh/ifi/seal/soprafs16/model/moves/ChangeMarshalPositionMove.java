29cc7ad56f2c23e99ce443014b29d943ce085a46
Sandro Wirth <s.wirth303@gmail.com>
2016-04-23 23:26:41 +0200

    implement target logic for SwitchMarshal, closes #46

--- src/main/java/ch/uzh/ifi/seal/soprafs16/model/moves/ChangeMarshalPositionMove.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs16/model/moves/ChangeMarshalPositionMove.java
@@ -3,9 +3,11 @@ package ch.uzh.ifi.seal.soprafs16.model.moves;
 import ch.uzh.ifi.seal.soprafs16.constant.ActionMoveType;
 import ch.uzh.ifi.seal.soprafs16.model.Move;
 import ch.uzh.ifi.seal.soprafs16.model.Target;
+import ch.uzh.ifi.seal.soprafs16.model.Wagon;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -23,7 +25,26 @@ public class ChangeMarshalPositionMove extends Move {
 
     @Override
     public List<Target> calculateTargets() {
-        //TODO calculate targets
-        return null;
+        List<Target> targets = new ArrayList<>();
+        List<Wagon> train = super.getGame().getTrain();
+
+        int position = 0;
+        for (Wagon w : train) {
+            if (w.hasMarshal()) {
+                break;
+            }
+            position++;
+        }
+
+        if (position == 0) {
+            targets.add(train.get(position + 1).getLowerLevel());
+        } else if (position == train.size() - 1) {
+            targets.add(train.get(position - 1).getLowerLevel());
+        } else {
+            targets.add(train.get(position - 1).getLowerLevel());
+            targets.add(train.get(position + 1).getLowerLevel());
+        }
+
+        return targets;
     }
 }
