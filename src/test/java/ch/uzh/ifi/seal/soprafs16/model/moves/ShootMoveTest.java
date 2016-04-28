cdea34137e2b7d495650881a150e5eb7fdbc85b2
sawirth <s.wirth303@gmail.com>
2016-04-28 08:24:01 +0200

    Split ShootMoveTest into more Test-Methods

--- src/test/java/ch/uzh/ifi/seal/soprafs16/model/moves/ShootMoveTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs16/model/moves/ShootMoveTest.java
@@ -3,7 +3,9 @@ package ch.uzh.ifi.seal.soprafs16.model.moves;
 import ch.uzh.ifi.seal.soprafs16.TestHelpers;
 import ch.uzh.ifi.seal.soprafs16.constant.CharacterType;
 import ch.uzh.ifi.seal.soprafs16.model.Game;
+import ch.uzh.ifi.seal.soprafs16.model.Move;
 import ch.uzh.ifi.seal.soprafs16.model.Target;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -14,64 +16,113 @@ import static org.hamcrest.Matchers.*;
 
 public class ShootMoveTest {
 
-    @Test
-    public void testCalculateTargets() throws Exception {
-        Game game = TestHelpers.createGame();
-        ShootMove move = new ShootMove();
-        move.setGame(game);
-        List<Target> targets = new ArrayList<>();
+    private Game game;
+    private Move move;
+    private List<Target> targets;
 
-        //Hans(1): can shoot on Fritz(6)
+    @Before
+    public void resetGame() {
+        /*
+        This creates a train with the following setup
+
+        Hans(1), Dave(2), Sandro(3)            Fritz(6)                 Sigmund(7)
+        --------------------------         ----------------------       -----------------
+
+               Sevi(4)                         Wayne(5)                   John(8)
+        --------------------------         ----------------------       ------------------
+
+
+         The number in the brackets is the ID and also the position in game.getPlayers()
+
+         */
+
+        game = TestHelpers.createGame();
+        move = new ShootMove();
+        move.setGame(game);
+        targets = new ArrayList<>();
+    }
+
+    @Test
+    public void testUpperLevelLoc() throws Exception {
+        //Hans(1) can shoot on Fritz
         move.setUser(game.getPlayers().get(0));
         targets = move.calculateTargets();
         assertThat(targets.size(), is(1));
         assertThat(targets.get(0).getId(), is(6L));
+    }
 
+    @Test
+    public void testLowerLevelLoc() throws Exception {
         //Sevi(4): can shoot on Wayne(5)
         move.setUser(game.getPlayers().get(3));
         targets = move.calculateTargets();
         assertThat(targets.size(), is(1));
         assertThat(targets.get(0).getId(), is(5L));
+    }
 
-        //Wayne(5): can shoot on Sevi(4) and John(8)
-        move.setUser(game.getPlayers().get(4));
-        targets = move.calculateTargets();
-        assertThat(targets.size(), is(2));
-        assertThat(targets.get(0).getId(), is(4L));
-        assertThat(targets.get(1).getId(), is(8L));
-
+    @Test
+    public void testUpperLevelMiddle() throws Exception {
         //Fritz(6): can shoot on Hans(1), Dave(2), Sandro(3) and Sigmund(7)
         move.setUser(game.getPlayers().get(5));
         targets = move.calculateTargets();
         assertThat(targets.size(), is(4));
 
-        //Special case Belle with Fritz(6): can shoot on Hans(1), Dave(2) and Sandro(3) as Sigmund plays as Belle
-        game.getPlayers().get(6).setCharacterType(CharacterType.BELLE);
-        targets = move.calculateTargets();
-        assertThat(targets.size(), is(3));
+    }
 
-        //Sigmund(7): can shoot on Fritz(6)
+    @Test
+    public void testLowerLevelMiddle() throws Exception {
+        //Wayne(5) can shoot on Sevi(4) and John(8)
+        move.setUser(game.getPlayers().get(4));
+        targets = move.calculateTargets();
+        assertThat(targets.size(), is(2));
+        assertThat(targets.get(0).getId(), is(4L));
+        assertThat(targets.get(1).getId(), is(8L));
+    }
+
+    @Test
+    public void testUpperLevelEnd() throws Exception {
+        //Sigmund(7) can shoot on Fritz(6)
         move.setUser(game.getPlayers().get(6));
         targets = move.calculateTargets();
         assertThat(targets.size(), is(1));
         assertThat(targets.get(0).getId(), is(6L));
+    }
 
-        //Sigmund(7) as Tuco: can shoot on Fritz(6) and John(8)
-        move.getUser().setCharacterType(CharacterType.TUCO);
-        targets = move.calculateTargets();
-        assertThat(targets.size(), is(2));
-        assertThat(targets.get(1).getId(), is(8L));
-
-        //John(8): can shoot on Wayne(5)
+    @Test
+    public void testLowerLevelEnd() throws Exception {
+        //John(8) can shoot on Wayne(5)
         move.setUser(game.getPlayers().get(7));
         targets = move.calculateTargets();
         assertThat(targets.size(), is(1));
         assertThat(targets.get(0).getId(), is(5L));
+    }
 
+    @Test
+    public void testTucoSpecialAbilityDownwards() throws Exception {
+        //Hans(1) can shoot on Fritz(6) and also on Sevi(4) below
+        move.setUser(game.getPlayers().get(1));
+        move.getUser().setCharacterType(CharacterType.TUCO);
+        targets = move.calculateTargets();
+        assertThat(targets.size(), is(2));
+    }
+
+    @Test
+    public void testTucoSpecialAbilityUpwards() throws Exception {
         //John(8) as Tuco: can shoot on Wayne(5) and Sigmund(7)
+        move.setUser(game.getPlayers().get(7));
         move.getUser().setCharacterType(CharacterType.TUCO);
         targets = move.calculateTargets();
         assertThat(targets.size(), is(2));
         assertThat(targets.get(1).getId(), is(7L));
+    }
+
+    @Test
+    public void testBelleSpecialAbility() throws Exception {
+        //Hans(1) now plays at Belle which means Fritz(6) can no longer shoot on him because there two other players on the same level
+        //he can only shoot on Dave(2), Sandro(3) and Sigmund(7)
+        move.setUser(game.getPlayers().get(5));
+        game.getPlayers().get(0).setCharacterType(CharacterType.BELLE);
+        targets = move.calculateTargets();
+        assertThat(targets.size(), is(3));
     }
 }
